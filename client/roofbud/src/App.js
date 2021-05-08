7f8b872c10d703e80e21ca4235eb5a7dd36491d6
ibarkay <ibarkay@gmail.com>
2021-05-08 17:27:38 +0300

    CSS has began, UI must be taken full attention now

--- client/roofbud/src/App.js
+++ client/roofbud/src/App.js
@@ -1,23 +1,57 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, HashRouter, Router } from "react-router-dom";
 
-import Header from "./pages/Header";
 import Login from "./pages/Login";
 import Sign from "./pages/Sign";
 import Profile from "./pages/Profile";
 import Match from "./pages/match";
+import Home from "./pages/Home";
+import "./CSS/app.css";
 
 const App = () => {
 	return (
-		<div>
-			<BrowserRouter>
-				<Header />
-				<Route path="/login" exact component={Login} />
-				<Route path="/sign" exact component={Sign} />
-				<Route path="/profile" exact component={Profile} />
-				<Route path="/match" exact component={Match} />
-			</BrowserRouter>
-		</div>
+		<HashRouter>
+			<div>
+				<ul className="header">
+					<li>
+						<a href="/#/home">Logo</a>
+					</li>
+					<li>
+						<a href="/#/login">login</a>
+					</li>
+					{/* <li>
+						<a href="/#/sign">sign</a>
+					</li> */}
+					{/* <li>
+						<a href="/#/profile">profile</a>
+					</li> */}
+					{/* <li>
+						<a href="/#/match">match</a>
+					</li> */}
+				</ul>
+				<div className="content">
+					  <Route path="/login" exact component={Login} />
+					<Route path="/sign" exact component={Sign} />
+					<Route path="/profile" exact component={Profile} />
+					<Route path="/match" exact component={Match} />
+					<Route path="/home" exact component={Home} />
+				</div>
+			</div>
+		</HashRouter>
 	);
 };
+
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<BrowserRouter>
+// 				<Header />
+// 				<Route path="/login" exact component={Login} />
+// 				<Route path="/sign" exact component={Sign} />
+// 				<Route path="/profile" exact component={Profile} />
+// 				<Route path="/match" exact component={Match} />
+// 			</BrowserRouter>
+// 		</div>
+// 	);
+// };
 
 export default App;
---
+++ client/roofbud/src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+	return (
+		<div className="home-container">
+			<h1>R00fBud - Perfect match!</h1>
+			<a href="/#/sign">Create account</a>
+		</div>
+	);
+};
+
+export default Home;
